f3b9cdc9ceb0edf72e203e0bceca434054d50e2e
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-09-14 18:45:48 +0100

    Support CORS in backend

--- backend/src/bin/web_server.rs
+++ backend/src/bin/web_server.rs
@@ -3,13 +3,28 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Error};
+
 use backend::rest::*;
 
-fn main() {
+fn main() -> Result<(), Error> {
+    let allowed_origins = AllowedOrigins::all();
+
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()?;
+
     rocket::ignite()
         .mount("/", routes![all_tasks::list, all_tasks::add, all_tasks::reset, 
                             single_task::show, single_task::edit, single_task::complete, single_task::remove,
                             stats::count,
                            ])
+        .attach(cors)
         .launch();
+
+    Ok(())
 }
