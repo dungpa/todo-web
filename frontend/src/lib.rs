8d86c8dd8a25586ea534957ea7893eee4f6a4ac7
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-09-14 13:43:44 +0100

    Bootstrap frontend MVU

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[macro_use]
+extern crate seed;
+use seed::prelude::*;
+
+#[derive(Clone, Debug)]
+enum Direction {
+    Coming,
+    Going,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct Model {
+    direction: Direction,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Clone, Debug)]
+enum Msg {
+    Click,
+}
+
+fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::Click => {
+            model.direction = match model.direction {
+                Direction::Coming => Direction::Going,
+                Direction::Going => Direction::Coming,
+            }
+        }
     }
 }
+
+fn view(model: &Model) -> impl View<Msg> {
+    let greeting = match model.direction {
+        Direction::Coming => "Hello, World!",
+        Direction::Going => "¡Hasta la vista!",
+    };
+    h1![
+        class! {"heading"},
+        style!["height" => "100vh",
+               "width" => "100vw",
+        ],
+        { greeting },
+        simple_ev(Ev::Click, Msg::Click),
+    ]
+}
+
+fn init(_url: Url, _orders: &mut impl Orders<Msg>) -> Model {
+    Model {
+        direction: Direction::Coming,
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn render() {
+    seed::App::build(init, update, view).finish().run();
+}
+
